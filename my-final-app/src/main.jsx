35a3384b93b2e0ab2d848312d7c69bbeda8d1fb6
VladHolovin27 <vladislavg899@gmail.com>
2025-06-04 20:17:08 +0300

    added 404.html

--- my-final-app/src/main.jsx
+++ my-final-app/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+    <BrowserRouter basename="/WeatherDashboard-/">
     <App />
+    </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
